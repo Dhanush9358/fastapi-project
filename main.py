5ca4d04454ca8041407013457fe75dcc51d242af
dhanushdarapureddy9358@gmail.com <dhanushdarapureddy9358@gmail.com>
2025-07-24 11:17:34 +0530

    main file update

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from database import Base, engine
 from routers import user, booking
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
+# Create tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -11,5 +13,11 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+# Root route
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Include routers
 app.include_router(user.router)
 app.include_router(booking.router)
