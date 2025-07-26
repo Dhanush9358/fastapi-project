a903804cd19caaa7f04df3cfd83ae3206df50ac2
dhanushdarapureddy9358@gmail.com <dhanushdarapureddy9358@gmail.com>
2025-07-26 15:01:57 +0530

    update database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
-import os
+# import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///./data.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
