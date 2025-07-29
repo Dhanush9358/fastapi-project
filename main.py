1851695713fca20228953c3a66b80cb85c8163eb
dhanushdarapureddy9358@gmail.com <dhanushdarapureddy9358@gmail.com>
2025-07-29 10:44:01 +0530

    update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from database import Base, engine
+from database import Base, engine, get_db
 from auth import router as auth_router
 from routers import booking, user
 from models import User, Booking
+from sqlalchemy.orm import Session
 
 Base.metadata.create_all(bind=engine)
 
@@ -15,6 +16,10 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", include_in_schema=False)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/test-db")
+def test_db(db: Session = Depends(get_db)):
+    return {"status": "Connected to PostgreSQL"}
 
 app.include_router(auth_router)
 app.include_router(user.router)
