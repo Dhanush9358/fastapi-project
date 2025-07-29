33248581e612ecb90be57e91e9155e7cf5d4ed3b
dhanushdarapureddy9358@gmail.com <dhanushdarapureddy9358@gmail.com>
2025-07-29 10:17:06 +0530

    add postegral for database

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
-# import os
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./data.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
