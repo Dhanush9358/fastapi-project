a006166200ecff3250837ccabff1e70341f62a90
dhanushdarapureddy9358@gmail.com <dhanushdarapureddy9358@gmail.com>
2025-07-30 15:52:21 +0530

    update database

--- database.py
+++ database.py
@@ -4,7 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-load_dotenv()
+if os.path.exists("/etc/secrets/.env"):
+    load_dotenv("/etc/secrets/.env")
+else:
+    load_dotenv()
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
