758924547de5b82f9d9eb03397c4648fae193453
dhanushdarapureddy9358@gmail.com <dhanushdarapureddy9358@gmail.com>
2025-08-02 20:59:12 +0530

    updae for reset

--- routers/user.py
+++ routers/user.py
@@ -21,11 +21,13 @@ conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
-    MAIL_TLS=True,
-    MAIL_SSL=False,
-    USE_CREDENTIALS=True
+    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME", "Room Booking App"),
+    MAIL_PORT=int(os.getenv("MAIL_PORT", 587)),
+    MAIL_SERVER=os.getenv("MAIL_SERVER", "smtp.gmail.com"),
+    MAIL_STARTTLS=os.getenv("MAIL_STARTTLS", "True").lower() == "true",
+    MAIL_SSL_TLS=os.getenv("MAIL_SSL_TLS", "False").lower() == "true",
+    USE_CREDENTIALS=os.getenv("USE_CREDENTIALS", "True").lower() == "true",
+    VALIDATE_CERTS=os.getenv("VALIDATE_CERTS", "True").lower() == "true"
 )
 
 # ------------------- Register -------------------
